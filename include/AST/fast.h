1699bd4ac981b289aca7ce380f47506781635389
George Potoshin <george.potoshin@gmail.com>
2021-11-28 17:50:54 +0300

    new file:   include/AST/fast.h 	new file:   tools/fancy-ast/CMakeLists.txt 	new file:   tools/fancy-ast/FancyAST.c

---
+++ include/AST/fast.h
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fast.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/28/2021 16:45:00
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:  George Potoshin (GP), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef FANCY_AST_FAST_H
+#define FANCY_AST_FAST_H
+
+#include <stdint.h>
+
+typedef uint32_t fast_stat;
+typedef void* info_t;
+typedef struct Fast_element fast_element;
+
+#define FAST_FANCTION 0x000000
+#define FAST_OPERATOR 0x000001
+#define FAST_VARIABLE 0x000002
+
+struct Fast_element{
+	fast_stat type;
+	info_t info;
+	fast_element next;
+};
+
+#endif
---
+++ tools/fancy-ast/FancyAST.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  FancyAST.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/28/2021 16:09:05
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:  George Potoshin (GP), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "AST/tree.h"
+s
